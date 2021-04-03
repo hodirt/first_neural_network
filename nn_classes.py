fdadf39c4e78982e6cbc8271a6903ae9b4daa862
hodirt <hodirt@gmail.com>
2021-04-03 19:41:50 +0300

    added backward propagation functions to Layer and ReLU activation classes

--- nn_classes.py
+++ nn_classes.py
@@ -5,19 +5,36 @@ class LayerDense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+        self.inputs = None
         self.output = None
+        self.dweights = None
+        self.dinputs = None
+        self.dbiases = None
 
     def forward(self, inputs):
+        self.inputs = inputs.copy()
         self.output = np.dot(inputs, self.weights) + self.biases
+
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 
 # ReLU - Rectified Linear Unit
 class ActivationReLU:
     def __init__(self):
+        self.inputs = None
         self.output = None
+        self.dinputs = None
 
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
 
 
 class ActivationSoftmax:
@@ -38,7 +55,15 @@ class CategoricalCrossEntropy:
         self.target_class = target_class
 
     def calculate_loss(self, predictions, classes):
+        """
+
+        :param predictions:
+        :param classes: - like, expected values
+        :return:
+        """
         if len(classes.shape) == 1:
+            # тут ми просто робимо 2д масив [[1, 0, 0], ...], де 1 позначає, де в предікшні мала би знаходитися
+            # відповідь правильна
             one_hot_vectors = np.zeros(predictions.shape)
             one_hot_vectors[range(len(predictions)), classes] = 1
         elif len(classes.shape) == 2:
